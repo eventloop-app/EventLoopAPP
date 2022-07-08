7d488978a5227550565e1aa4f7186ddeb30e3bd8
PorPlaTakKom <apiwat.sri@icloud.com>
2022-07-09 05:08:47 +0700

    [ADD] EventCard

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,19 +1,39 @@
-import {SafeAreaView, Text} from "react-native";
+import {FlatList, SafeAreaView, Text, View} from "react-native";
 import React, { useEffect, useState } from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import EventCard from "../components/EventCard";
-import {SignInButtons} from "../components/SignInButtons";
-const Tab = createBottomTabNavigator();
+import eventsService from "../services/eventsService";
 
 const FeedScreen = ({ route, navigation }) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [events, setEvent] = useState([])
+
+  //ดึง Event ทั่งหมด
+  useEffect(()=>{
+    eventsService.getEventAll().then( async res => {
+      await setEvent(res.data.content)
+      await setIsLoading(false)
+    }).catch(error => {
+      console.log('get_all_event: ' + error)
+    })
+  },[])
 
   return (
     <SafeAreaView style={{flex:1,backgroundColor: 'white'}}>
-      <EventCard/>
-      <SignInButtons msg={"TESTAPI"}/>
+      {
+        isLoading ? null :
+          <View>
+            <FlatList
+              data={events}
+              renderItem={EventCard}
+              keyExtractor={(item) => item.id}
+              showsHorizontalScrollIndicator={false}
+              horizontal={true}
+            />
+          </View>
+      }
     </SafeAreaView>
   )
 }
-
 
 export default FeedScreen;
