906c233b6a2c36a3f63d86397915f0624fdb2d19
PorPlaTakKom <apiwat.sri@icloud.com>
2022-07-26 16:00:18 +0700

    [UPDATE] MapScreen.js

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -273,7 +273,6 @@ const EditProfileScreen = (props) => {
             </ProgressSteps>
           </View>)
       }
-
     </SafeAreaView>
   )
 }
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import MapView, {Marker} from 'react-native-maps';
-import {StyleSheet, Text, View, Dimensions, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, SafeAreaView, TextInput, TouchableOpacity, Platform} from 'react-native';
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
 import * as Location from 'expo-location';
 import {createRef, useEffect, useState} from "react";
+import fonts from "../constants/Fonts";
+import fontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
 
 const MapScreen = () => {
     const [location, setLocation] = useState({
@@ -13,8 +16,13 @@ const MapScreen = () => {
         longitudeDelta: 0.34865230321884155
     })
     const mapRef = createRef();
-
-    const [marker, setMarker] = useState({ lat: 0, lng: 0 });
+    const mapRef2 = createRef();
+    const [mapData, setMapData] = useState({
+        name: null,
+        lat: null,
+        lng: null,
+    })
+    const [marker, setMarker] = useState({lat: 0, lng: 0});
 
     useEffect(() => {
         (async () => {
@@ -29,47 +37,81 @@ const MapScreen = () => {
                 latitude: nowLocation.coords.latitude,
                 longitude: nowLocation.coords.longitude
             })
+
         })();
     }, []);
 
-    useEffect(()=>{
-        if(marker.lat !== 0){
+    useEffect(() => {
+        if (marker.lat !== 0) {
             mapRef.current.animateToRegion({
                 latitude: marker.lat,
                 longitude: marker.lng,
-                longitudeDelta: 0.03,
-                latitudeDelta: 0.03
+                longitudeDelta: 0.001,
+                latitudeDelta: 0.001
             })
         }
-    },[marker])
+
+
+    }, [marker])
 
     return (
         <View style={styles.container}>
             <View>
                 <MapView
+
+                    showsMyLocationButton={true}
                     ref={mapRef}
                     moveOnMarkerPress={true}
-                    onMarkerDrag={()=> console.log('Mark')}
+                    onMarkerDrag={() => console.log('Mark')}
                     style={styles.maps}
                     provider={"google"}
                     showsUserLocation={true}
                     initialRegion={location}
                     followsUserLocation={true}
-                    onPoiClick={(a) => console.log(a.nativeEvent)}
-                >
-                        <Marker onPress={()=> console.log('asdasd')}  description={"บันทาว"} coordinate={{ latitude: marker.lat, longitude: marker.lng }} />
+                    onPoiClick={(a) => {
+                        setMarker({
+                            lat: a.nativeEvent.coordinate.latitude,
+                            lng: a.nativeEvent.coordinate.longitude,
+                        })
+                        setMapData({
+                            lat: a.nativeEvent,
+                            lng: a.nativeEvent.coordinate.longitude,
+                            name: a.nativeEvent.name
+                        })
+                    }}>
+                    <Marker image={'https://cdn.discordapp.com/emojis/855437648718069771.webp?size=96&quality=lossless'}
+                            description={mapData?.name} coordinate={{latitude: marker.lat, longitude: marker.lng}}/>
                 </MapView>
-                <View style={{position: 'absolute', top: 44, width: '90%', left: "5%"}}>
+                <View style={{
+                    position: 'absolute',
+                    top: (Platform.OS === "android" ? 70 : 50),
+                    width: '90%',
+                    left: "5%",
+                    zIndex: 10
+                }}>
                     <GooglePlacesAutocomplete
+                        ref={mapRef2}
+                        styles={{
+                            textInput: {
+                                fontFamily: fonts.primary,
+                                fontSize: fontSize.primary
+                            },
+                        }}
+                        placeholder={'ค้นหาสถานที่'}
                         autoFocus={true}
                         fetchDetails={true}
                         onPress={(data, details = null) => {
-                            // console.log(data, details)
                             setMarker({
+                                ...marker,
                                 lat: details.geometry.location.lat,
                                 lng: details.geometry.location.lng,
                             })
-
+                            setMapData({
+                                ...mapData,
+                                lat: details.geometry.location.lat,
+                                lng: details.geometry.location.lng,
+                                name: data.description
+                            })
                         }}
                         nearbyPlacesAPI='GooglePlacesSearch'
                         GooglePlacesSearchQuery={{
@@ -84,6 +126,57 @@ const MapScreen = () => {
 
                         }}
                     />
+                </View>
+                <View style={{
+                    width: "100%",
+                    height: 0,
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                    <View style={{
+                        position: 'absolute',
+                        top: 550,
+                        width: 200,
+                        height: 125,
+                        backgroundColor: 'rgba(255,255,255,0.7)',
+                        borderRadius: 12,
+
+                    }}>
+                        <Text
+                            numberOfLines={2}
+                            style={{
+                                padding: 5,
+                                textAlign: 'center',
+                                fontFamily: fonts.primary,
+                                fontSize: fontSize.small
+                            }}>{mapData?.name}</Text>
+                        <View style={{display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <TouchableOpacity
+                                style={{
+                                    borderRadius: 5,
+                                    height: 35,
+                                    width: 120,
+                                    backgroundColor: Colors.primary,
+                                    justifyContent: 'center'
+                                }}>
+                                <Text
+                                    style={{
+                                        color: Colors.white,
+                                        textAlign: 'center',
+                                        fontFamily: fonts.bold,
+                                        fontSize: fontSize.small
+                                    }}
+                                >
+                                    ยืนยันสถานที่
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
                 </View>
             </View>
         </View>
